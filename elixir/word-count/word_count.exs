4491b5fb0157dbd7286b3b66d80b4148c2962f2f
Dean Chouinard <deanchouinard@gmail.com>
2016-07-20 08:49:38 -0400

    working on word_count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -7,5 +7,33 @@ defmodule Words do
   @spec count(String.t) :: map
   def count(sentence) do
 
+    String.split(sentence)
+    |> IO.inspect
+    |> Enum.reduce(%{}, &cnt_word/2)
   end
+
+  def cnt_word(word, word_list) when word !== nil do
+    word = clean_word(word)
+    case Map.fetch(word_list, word) do
+      {:ok, _} ->
+        Map.update!(word_list, word, &(&1 + 1))
+      :error ->
+        # IO.puts "Word: #{inspect word}"
+        # IO.puts "Word_list: #{inspect word_list}"
+        if String.match?(word, ~r/[\d\w]+/) do
+          Map.put(word_list, word, 1)
+        else
+          word_list
+        end
+    end
+  end
+  def cnt_word(_, word_list), do: word_list
+
+  defp clean_word(word) do
+    word = String.downcase(word)
+    word = String.replace(word, ~r/[\W]/, "")
+  end
+
 end
+# Words.count("hello")
+
