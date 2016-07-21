02b76005a96185b0f9cc743e022c9bbc1b5659b1
Dean Chouinard <deanchouinard@gmail.com>
2016-07-20 22:59:14 -0400

    refactored word_count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,35 +6,12 @@ defmodule Words do
   """
   @spec count(String.t) :: map
   def count(sentence) do
-
     String.split(sentence, [" ", "_"])
-    |> Enum.reduce(%{}, &cnt_word/2)
+    |> Stream.filter(fn(word) -> String.match?(word, ~r/[\d\w]+/) end)
+    |> Stream.map(fn(word) -> String.downcase(word) end)
+    |> Stream.map(fn(word) -> String.replace(word, ~r/[^-\w]/u, "") end)
+    |> Enum.reduce(%{}, fn(word, word_list) -> Map.update(word_list, 
+                          word, 1, &(&1 + 1)) end)
   end
-
-  def cnt_word(word, word_list) when word !== nil do
-    # IO.puts "Word: #{inspect word}"
-    word = clean_word(word)
-    case Map.fetch(word_list, word) do
-      {:ok, _} ->
-        Map.update!(word_list, word, &(&1 + 1))
-      :error ->
-        # IO.puts "Word: #{inspect word}"
-        # IO.puts "Word_list: #{inspect word_list}"
-        if String.match?(word, ~r/[\d\w]+/) do
-          Map.put(word_list, word, 1)
-        else
-          word_list
-        end
-    end
-  end
-  def cnt_word(_, word_list), do: word_list
-
-  defp clean_word(word) do
-    String.downcase(word)
-    |> String.replace(~r/[^-\w]/u, "")
-    # String.replace(word, ~r/[^-\w]/, "")
-  end
-
 end
-# Words.count("hello")
 
