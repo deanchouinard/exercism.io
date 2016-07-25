7156cd221b2e853e89707d53d2f13203fc711f2b
Dean Chouinard <deanchouinard@gmail.com>
2016-07-25 18:01:05 -0400

    trying to split camel case words

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -6,6 +6,8 @@ defmodule Acronym do
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
     String.split(string)
+    #    |> Enum.each(&(String.split(&1, ~r/(?=[A-Z])/)))
+    |> IO.inspect
     |> Enum.reduce("", fn(x, abv) -> abv <> String.upcase(String.first(x)) end)
   end
 end
