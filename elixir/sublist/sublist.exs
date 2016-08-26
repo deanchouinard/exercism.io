af887938efb95f2bd27a9c6eff53de9c44de2801
Dean Chouinard <deanchouinard@gmail.com>
2016-08-25 22:27:10 -0400

    finished sublist

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,40 +3,74 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
+  def compare([], []), do: :equal
+  def compare([], [nil]), do: :sublist
+  def compare([nil], []), do: :superlist
   def compare(a, b) do
-    _compare(a, b)
-  end
-
-  def _compare([], []), do: :equal
-  def _compare([], [nil]), do: :sublist
-  def _compare([nil], []), do: :superlist
-  def _compare(a, b) do
-    cond do
-      a == b -> :equal
-      true -> compare2(a, b)
-
-      #true -> scompare(a, b, [], [])
+    if a == b do
+      :equal
+    else
+      if _compare(a, b) do
+        :sublist
+      else
+        if _compare(b, a) do
+          :superlist
+        else
+          :unequal
+        end
+      end
     end
   end
 
-  def compare2(a, b) do
-    _compare2(a, b, false)
-  end
+  #  def _compare(_, []), do: false
+  def _compare(a, b) do
+    a_count = Enum.count(a)
+    b_count = Enum.count(b)
 
-  def _compare2([], _, false), do: :unequal
-  def _compare2([], _, true), do: :sublist
-  def _compare2(_, [], false), do: :unequal
-  def _compare2(_, [], true), do: :unequal
-  def _compare2(a, b, match) do
-    if hd(a) == hd(b) do
-      match = true
-      _compare2(tl(a), tl(b), match)
+    if a === Enum.slice(b, 0, a_count) do
+      true
     else
-      _compare2(a, tl(b), match)
+      if a_count > b_count do
+        false
+      else
+        _compare(a, tl(b))
+      end
     end
   end
 end
-
+#
+#   def compare2(a, b) do
+#     if _compare2(a, b, false, a) do
+#       :sublist
+#     else
+#       if _compare2(b, a, false, b) do
+#         :superlist
+#       else
+#         :unequal
+#       end
+#     end
+#   end
+#
+#   def _compare2([], _, false, _), do: false
+#   def _compare2([], _, true, _), do: true
+#   def _compare2(_, [], false, _), do: false
+#   def _compare2(_, [], true, _), do: false 
+#   def _compare2(a, b, match, ref) do
+#     if hd(a) === hd(b) do
+#       match = true
+#       _compare2(tl(a), tl(b), match, ref)
+#     else
+#       if match do
+#         match = false
+#         _compare2(ref, tl(b), match, ref)
+#       else
+#         _compare2(a, tl(b), match, ref)
+#       end
+#
+#     end
+#   end
+# end
+#
 #   def scompare([], _, _, _), do: :unequal
 #   def scompare(_, [], _, _), do: :unequal
 #   def scompare([heada | taila] = a, [headb | tailb] = b, suba, subb) do
